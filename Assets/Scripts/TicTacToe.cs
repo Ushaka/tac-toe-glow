0485616b00692dd5566ddf7e53cda6e1486f1ceb
Snorre Davøen <snorremd@gmail.com>
2014-02-12 18:51:52 +0100

    Initial game logic and game state object implemented.

---
+++ Assets/Scripts/TicTacToe.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TicTacToe : MonoBehaviour {
+
+	public GameObject[,] cells;
+
+	// Use this for initialization
+	void Start () {
+		// Initialize the map
+		cells = new GameObject[3,3];
+		cells [0, 0] = GameObject.Find ("Cell1").GetComponent<TriggerCell> ().playerType;
+		cells [0, 1] = GameObject.Find ("Cell2").GetComponent<TriggerCell> ().playerType;
+		cells [0, 2] = GameObject.Find ("Cell3").GetComponent<TriggerCell> ().playerType;
+		cells [1, 0] = GameObject.Find ("Cell4").GetComponent<TriggerCell> ().playerType;
+		cells [1, 1] = GameObject.Find ("Cell5").GetComponent<TriggerCell> ().playerType;
+		cells [1, 2] = GameObject.Find ("Cell6").GetComponent<TriggerCell> ().playerType;
+		cells [2, 0] = GameObject.Find ("Cell7").GetComponent<TriggerCell> ().playerType;
+		cells [2, 1] = GameObject.Find ("Cell8").GetComponent<TriggerCell> ().playerType;
+		cells [2, 2] = GameObject.Find ("Cell9").GetComponent<TriggerCell> ().playerType;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Return) {
+			string winner = findWinner();
+
+		}
+	
+	}
+	
+	private string findWinner() {
+
+		if(playerWon("x")) {
+			return "x";
+
+		} else if(playerWon("o")) {
+			return "o";
+
+		} else {
+			return "t";
+		}
+
+	}
+
+	private bool playerWon(string player) {
+		// check rows
+		if (cells[0, 0] == player && cells[0, 1] == player && cells[0, 2] == player) { return true; }
+		if (cells[1, 0] == player && cells[1, 1] == player && cells[1, 2] == player) { return true; }
+		if (cells[2, 0] == player && cells[2, 1] == player && cells[2, 2] == player) { return true; }
+		
+		// check columns
+		if (cells[0, 0] == player && cells[1, 0] == player && cells[2, 0] == player) { return true; }
+		if (cells[0, 1] == player && cells[1, 1] == player && cells[2, 1] == player) { return true; }
+		if (cells[0, 2] == player && cells[1, 2] == player && cells[2, 2] == player) { return true; }
+		
+		// check diags
+		if (cells[0, 0] == player && cells[1, 1] == player && cells[2, 2] == player) { return true; }
+		if (cells[0, 2] == player && cells[1, 1] == player && cells[2, 0] == player) { return true; }
+
+		return true;
+
+	}
+}
---
+++ Assets/Scripts/TriggerCell.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCell : MonoBehaviour {
+
+	public string playerType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/**
+	 * Update cell state with correct player type 
+	 */
+	void OnCollisionStay(Collider other) {
+
+		if(other.gameObject.name.Equals("Circle")) {
+			playerType = "o";
+		} else if(other.gameObject.name.Equals("WholeX")) {
+			playerType = "x";
+		}
+	}
+}
